7b50c0c13937412bb154a0124f3d3c8b88156d1d
Gregor Ihmor <gregor_alias1@outlook.de>
2016-12-27 20:36:16 +0100

    added headers

--- src/Starter.scala
+++ src/Starter.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016  Gregor Ihmor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import operators.{Operators, Variables}
 
 object VariablesX extends Variables[Double] {
@@ -29,7 +46,7 @@ object Starter {
     import spire.implicits._
     import spire.math.Complex
     val p = parser.parser[Complex[Double]]
-    println(p("i", Variables.emptyVariables))
+    println(p("sin(25 + 3*i)", Variables.emptyVariables))
   }
 
   def main(args: Array[String]): Unit = {
