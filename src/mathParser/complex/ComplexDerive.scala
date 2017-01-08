7cb3a0e7214183412065a60ccfe29c94a457a45d
Gregor Ihmor <gregor_alias1@outlook.de>
2017-01-08 15:25:40 +0100

    added missing deriving rules

--- src/mathParser/complex/ComplexDerive.scala
+++ src/mathParser/complex/ComplexDerive.scala
@@ -28,6 +28,9 @@ object ComplexDerive extends Derive[ComplexLanguage.type] with ComplexSyntaxSuga
       case Variable(_) | Constant(_) => constant(0d)
       case UnitaryNode(Neg, child) => neg(derive(child))
       case UnitaryNode(Sin, child) => times(derive(child), cos(child))
+      case UnitaryNode(Atan, child) => divided(derive(child), plus(constant(1d), times(child, child)))
+      case in@UnitaryNode(Exp, child) => times(in, derive(child))
+        // todo: there are functions missing.
       case BinaryNode(Plus, t1, t2) => plus(derive(t1), derive(t2))
       case BinaryNode(Minus, t1, t2) => minus(derive(t1), derive(t2))
       case BinaryNode(Times, t1, t2) => plus(times(derive(t1), t2), times(derive(t2), t1))
--- src/mathParser/double/DoubleDerive.scala
+++ src/mathParser/double/DoubleDerive.scala
@@ -29,6 +29,8 @@ object DoubleDerive extends Derive[DoubleLanguage.type] with DoubleSyntaxSugar {
       case Variable(_) | Constant(_) => constant(0d)
       case UnitaryNode(Neg, child) => neg(derive(child))
       case UnitaryNode(Sin, child) => times(derive(child), cos(child))
+      case UnitaryNode(Atan, child) => divided(derive(child), plus(constant(1d), times(child, child)))
+      case in@UnitaryNode(Exp, child) => times(in, derive(child))
       case BinaryNode(Plus, t1, t2) => plus(derive(t1), derive(t2))
       case BinaryNode(Minus, t1, t2) => minus(derive(t1), derive(t2))
       case BinaryNode(Times, t1, t2) => plus(times(derive(t1), t2), times(derive(t2), t1))
