a23fdc826cf3ffd8f5bda064ed67162e0ca6f32e
Merlin Göttlinger <megoettlinger@gmail.com>
2017-01-10 21:02:52 +0100

    Experimental path dependent AST

--- src/mathParser/Language.scala
+++ src/mathParser/Language.scala
@@ -27,4 +27,10 @@ trait Language {
   def binaryOperators: Seq[BinaryOperator]
   def binaryInfixOperators: Seq[BinaryOperator]
   def constants(): Seq[Constant]
+  
+  sealed trait Node
+  case class Constant(value:Skalar) extends Node
+  case class UnitaryNode(op:UnitaryOperator, child:Node) extends Node
+  case class BinaryNode(op:BinaryOperator, childLeft:Node, childRight:Node) extends Node
+  case class Variable(name: Symbol) extends Node
 }
