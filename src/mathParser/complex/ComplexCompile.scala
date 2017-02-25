4aa11c72f6a31ab44de4138171acee2484be9e32
Gregor Ihmor <gregor_alias1@outlook.de>
2017-02-25 17:57:20 +0100

    refactoring

--- src/mathParser/complex/ComplexCompile.scala
+++ src/mathParser/complex/ComplexCompile.scala
@@ -25,32 +25,32 @@ import scala.annotation.tailrec
 import scala.util.Try
 
 object ComplexCompile extends Compile[ComplexLanguage.type] {
+  def scalaCode(node: Node[ComplexLanguage.type]): String = node match {
+    case Constant(v) => s"Complex[Double](${v.real}, ${v.imag})"
+    case UnitaryNode(Neg, child) => s"-(${scalaCode(child)})"
+    case UnitaryNode(Sin, child) => s"(${scalaCode(child)}).sin"
+    case UnitaryNode(Cos, child) => s"(${scalaCode(child)}).cos"
+    case UnitaryNode(Tan, child) => s"(${scalaCode(child)}).tan"
+    case UnitaryNode(Asin, child) => s"(${scalaCode(child)}).asin"
+    case UnitaryNode(Acos, child) => s"(${scalaCode(child)}).acos"
+    case UnitaryNode(Atan, child) => s"(${scalaCode(child)}).atan"
+    case UnitaryNode(Sinh, child) => s"(${scalaCode(child)}).sinh"
+    case UnitaryNode(Cosh, child) => s"(${scalaCode(child)}).cosh"
+    case UnitaryNode(Tanh, child) => s"(${scalaCode(child)}).tanh"
+    case UnitaryNode(Exp, child) => s"(${scalaCode(child)}).exp"
+    case UnitaryNode(Log, child) => s"(${scalaCode(child)}).log"
+
+    case BinaryNode(Plus, left, right) => s"((${scalaCode(left)}) + (${scalaCode(right)}))"
+    case BinaryNode(Minus, left, right) => s"((${scalaCode(left)}) - (${scalaCode(right)}))"
+    case BinaryNode(Times, left, right) => s"((${scalaCode(left)}) * (${scalaCode(right)}))"
+    case BinaryNode(Divided, left, right) => s"((${scalaCode(left)}) / (${scalaCode(right)}))"
+    case BinaryNode(Power, left, right) => s"((${scalaCode(left)}) ** (${scalaCode(right)}))"
+
+    case Variable(name) => name.name
+  }
+
   override def apply(node: Node[ComplexLanguage.type])(v1: Variable): Try[Complex[Double] => Complex[Double]] =
     Try {
-      def functionString(node: Node[ComplexLanguage.type]): String = node match {
-        case Constant(v) => s"Complex[Double](${v.real}, ${v.imag})"
-        case UnitaryNode(Neg, child) => s"-(${functionString(child)})"
-        case UnitaryNode(Sin, child) => s"(${functionString(child)}).sin"
-        case UnitaryNode(Cos, child) => s"(${functionString(child)}).cos"
-        case UnitaryNode(Tan, child) => s"(${functionString(child)}).tan"
-        case UnitaryNode(Asin, child) => s"(${functionString(child)}).asin"
-        case UnitaryNode(Acos, child) => s"(${functionString(child)}).acos"
-        case UnitaryNode(Atan, child) => s"(${functionString(child)}).atan"
-        case UnitaryNode(Sinh, child) => s"(${functionString(child)}).sinh"
-        case UnitaryNode(Cosh, child) => s"(${functionString(child)}).cosh"
-        case UnitaryNode(Tanh, child) => s"(${functionString(child)}).tanh"
-        case UnitaryNode(Exp, child) => s"(${functionString(child)}).exp"
-        case UnitaryNode(Log, child) => s"(${functionString(child)}).log"
-
-        case BinaryNode(Plus, left, right) => s"((${functionString(left)}) + (${functionString(right)}))"
-        case BinaryNode(Minus, left, right) => s"((${functionString(left)}) - (${functionString(right)}))"
-        case BinaryNode(Times, left, right) => s"((${functionString(left)}) * (${functionString(right)}))"
-        case BinaryNode(Divided, left, right) => s"((${functionString(left)}) / (${functionString(right)}))"
-        case BinaryNode(Power, left, right) => s"((${functionString(left)}) ** (${functionString(right)}))"
-
-        case Variable(name) => name.name
-      }
-
       import reflect.runtime.currentMirror
       import tools.reflect.ToolBox
       val toolbox = currentMirror.mkToolBox()
@@ -59,7 +59,7 @@ object ComplexCompile extends Compile[ComplexLanguage.type] {
            |import spire.math.Complex
            |
            |new Function1[Complex[Double], Complex[Double]]{
-           |  def apply(${v1.name}:Complex[Double]):Complex[Double] = ${functionString(node)}
+           |  def apply(${v1.name}:Complex[Double]):Complex[Double] = ${scalaCode(node)}
            |}
       """.stripMargin
 
