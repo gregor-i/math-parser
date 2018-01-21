b30ccea9c3da94bb5f2c57f194561a0e05492086
gregor <gregor_alias1@outlook.de>
2018-01-21 19:31:27 +0100

    created a minimal cli example

--- src/mathParser/slices/Evaluate.scala
+++ src/mathParser/slices/Evaluate.scala
@@ -5,7 +5,7 @@ package mathParser.slices
 trait Evaluate {
   _: LanguageOperators with AbstractSyntaxTree =>
 
-  private[mathParser] def evaluate(node: Node)
+  def evaluate(node: Node)
                                   (variableAssignment: PartialFunction[Symbol, S]): S =
     node.fold[S](identity,
       (op, child) => op.apply(child),
