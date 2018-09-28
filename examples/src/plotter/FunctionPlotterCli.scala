55b478ae5938cf8b734dc624244a8ea169992d0f
Gregor Ihmor <gregor_alias1@outlook.de>
2018-09-28 10:38:11 +0200

    simplified the examples

--- examples/src/plotter/FunctionPlotterCli.scala
+++ examples/src/plotter/FunctionPlotterCli.scala
@@ -1,12 +1,32 @@
 package plotter
 
-import mathParser.MathParser
+import java.io.File
 
+import mathParser.MathParser
+import org.jfree.chart.ChartPanel
 import scalax.chart.api._
 
-case class Config(term: String = null)
+case class Config(term: String = null,
+                  out: File = new File("chart.png"),
+                  width: Int = ChartPanel.DEFAULT_WIDTH * 2,
+                  height: Int = ChartPanel.DEFAULT_HEIGHT * 2)
 
-object ConfigParser extends scopt.OptionParser[Config]("scopt") {
+object ConfigParser extends scopt.OptionParser[Config]("function plotter cli") {
+  opt[File]('o', "out")
+    .optional()
+    .valueName("<file>")
+    .action((x, c) => c.copy(out = x))
+
+  opt[Int]('w', "width")
+    .optional()
+    .valueName("<width in px>")
+    .action((x, c) => c.copy(width = x))
+
+  opt[Int]('h', "height")
+    .optional()
+    .valueName("<height in px>")
+    .action((x, c) => c.copy(height = x))
+
   arg[String]("term")
     .required()
     .validate(term => MathParser.doubleLanguage('x).parse(term)
@@ -21,19 +41,21 @@ object Main {
       val lang = MathParser.doubleLanguage('x)
       val parsed = lang.parse(config.term).get
       val f = lang.compile1(parsed).get
+      val `f'` = lang.compile1(lang.derive(parsed)('x)).get
 
       val p1 = new XYSeries(s"f(x)": Comparable[_], false, true)
+      val p2 = new XYSeries(s"f'(x)": Comparable[_], false, true)
       val ps = new XYSeriesCollection()
       ps.addSeries(p1)
+      ps.addSeries(p2)
 
       for (x <- -10d to 10d by 0.1) {
-        p1.add(x, lang.evaluate(parsed){case 'x => x})
+        p1.add(x, f(x))
+        p2.add(x, `f'`(x))
       }
 
       val chart = XYLineChart(ps)
-      chart.title = s"f(x) = ${config.term}"
-      chart.show()
-      //    chart.saveAsPNG(config.out.getAbsolutePath, (config.width, config.height))
+      chart.saveAsPNG(config.out.getAbsolutePath, (config.width, config.height))
     }
   }
 }
